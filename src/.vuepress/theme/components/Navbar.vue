3a089b80721284ad4f4b86b576864e7c19a31cc0
takoba <plane25@gmail.com>
2019-05-14 22:02:38 +0900

    :bug: home以外のページでnavbarが透明になっているのを修正

--- src/.vuepress/theme/components/Navbar.vue
+++ src/.vuepress/theme/components/Navbar.vue
@@ -111,6 +111,7 @@ $navbar-vertical-padding = 0.7rem
 $navbar-horizontal-padding = 1.5rem
 
 .navbar
+  background white
   padding $navbar-vertical-padding $navbar-horizontal-padding
   line-height $navbarHeight - 1.4rem
   a, span, img
