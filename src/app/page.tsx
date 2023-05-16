38b7f89e024645f5eccf6230aec546e0d905ced1
takoba <plane25@gmail.com>
2023-05-17 03:48:46 +0900

    :sparkles: implement hero in toppage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,54 @@
 import Image from "next/image"
-import { Inter } from "next/font/google"
+import { Inter, Noto_Sans_Javanese } from "next/font/google"
+import { CSSProperties } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
+
+const notoSansJpH1 = Noto_Sans_Javanese({ subsets: ["latin"], weight: ["700"] })
+const notoSansJpText = Noto_Sans_Javanese({ subsets: ["latin"] })
+
+const styles: { [key: string]: CSSProperties } = {
+  hero: {
+    height: "480px",
+    backgroundColor: "#F9E5C0",
+  },
+}
 
 const Home = (): JSX.Element => {
   return (
     <>
-      <header>
-        <Image
-          src="/logo.svg"
-          alt="takoba (signed by takoba)"
-          width={480}
-          height={180}
-        />
-      </header>
       <main>
-        <a href="https://github.com/takoba">
-          <span className={`${inter.className}`}>
-            https://github.com/takoba
-          </span>
-        </a>
+        <div
+          className="hero m-auto flex justify-end items-end"
+          style={styles.hero}
+        >
+          <Image
+            src="/logo.svg"
+            alt="takoba (signed by takoba)"
+            width={960}
+            height={360}
+            className="-rotate-3 p-4 absolute top-0 left-0"
+          />
+          <div className="p-4 mr-16">
+            <h1 className={`${notoSansJpH1.className} text-6xl`}>
+              オバラタクヤ
+            </h1>
+            <p className={`${notoSansJpText.className}`}>
+              Software Engineer OR UX Designer
+            </p>
+          </div>
+        </div>
+        <div className={`${notoSansJpText.className}`}>
+          <a href="https://github.com/takoba">
+            <span>https://github.com/takoba</span>
+          </a>
+        </div>
       </main>
-      <footer>©2023 takoba</footer>
+      <footer>
+        <p className={`${notoSansJpText.className} w-full py-2 text-center`}>
+          ©2023 takoba
+        </p>
+      </footer>
     </>
   )
 }
