68e37ebfeab6ca5714dabe6bac3e222c4a19844f
takoba <plane25@gmail.com>
2023-05-17 05:32:06 +0900

    :construction: WIP: build a links section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,71 @@
-import Image from "next/image"
-import { Inter, Noto_Sans_Javanese } from "next/font/google"
-import { CSSProperties } from "react"
+"use client"
 
-const inter = Inter({ subsets: ["latin"] })
+import React, { Suspense } from "react"
+import Image from "next/image"
+import NextLink from "next/link"
+import { Noto_Sans_Javanese } from "next/font/google"
+import { Link as MuiLink } from "@mui/material"
+import GitHubIcon from "@mui/icons-material/GitHub"
 
 const notoSansJpH1 = Noto_Sans_Javanese({ subsets: ["latin"], weight: ["700"] })
+const notoSansJpH2 = Noto_Sans_Javanese({ subsets: ["latin"], weight: ["600"] })
 const notoSansJpText = Noto_Sans_Javanese({ subsets: ["latin"] })
 
-const styles: { [key: string]: CSSProperties } = {
-  hero: {
-    height: "480px",
-    backgroundColor: "#F9E5C0",
-  },
-}
+const Hero = (): JSX.Element => (
+  <>
+    <div
+      className="h-screen m-auto flex justify-end items-end"
+      style={{
+        backgroundColor: "#F9E5C0",
+      }}
+    >
+      <Image
+        src="/logo.svg"
+        alt="takoba (signed by takoba)"
+        width={960}
+        height={360}
+        className="-rotate-3 p-4 absolute top-0 left-0"
+      />
+      <div className="p-4 mr-16">
+        <h1 className={`${notoSansJpH1.className} text-6xl`}>オバラタクヤ</h1>
+        <p className={`${notoSansJpText.className}`}>
+          Software Engineer OR UX Designer
+        </p>
+      </div>
+    </div>
+  </>
+)
+
+const Headline = ({ children }: React.PropsWithChildren): JSX.Element => (
+  <h2 className={`${notoSansJpH2.className} text-2xl`}>
+    <span className="mr-2">#</span>
+    {children}
+  </h2>
+)
 
 const Home = (): JSX.Element => {
   return (
     <>
       <main>
-        <div
-          className="hero m-auto flex justify-end items-end"
-          style={styles.hero}
+        <Hero />
+        <section
+          id="links"
+          className={`${notoSansJpText.className} w-full px-16 py-8`}
         >
-          <Image
-            src="/logo.svg"
-            alt="takoba (signed by takoba)"
-            width={960}
-            height={360}
-            className="-rotate-3 p-4 absolute top-0 left-0"
-          />
-          <div className="p-4 mr-16">
-            <h1 className={`${notoSansJpH1.className} text-6xl`}>
-              オバラタクヤ
-            </h1>
-            <p className={`${notoSansJpText.className}`}>
-              Software Engineer OR UX Designer
-            </p>
+          <NextLink href="#links">
+            <Headline>Links</Headline>
+          </NextLink>
+          <div className="p-2 ml-4">
+            <ul className="list-disc">
+              <li>
+                <GitHubIcon className="mr-2" />
+                <MuiLink href="https://github.com/takoba" component={NextLink}>
+                  @takoba
+                </MuiLink>
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className={`${notoSansJpText.className}`}>
-          <a href="https://github.com/takoba">
-            <span>https://github.com/takoba</span>
-          </a>
-        </div>
+        </section>
       </main>
       <footer>
         <p className={`${notoSansJpText.className} w-full py-2 text-center`}>
