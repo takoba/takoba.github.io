2d88a66d6b8185d0164867c4c9411869cd807737
takoba <plane25@gmail.com>
2023-06-05 06:11:07 +0900

    :lipstick: Fix `<Hero />` component (CSS Strategy, etc.)

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,30 +2,63 @@
 "use client"
 
 import React from "react"
-import { css } from "@emotion/react"
 import Image from "next/image"
 import { Noto_Sans_Javanese } from "next/font/google"
 
 const notoSansJpH1 = Noto_Sans_Javanese({ subsets: ["latin"], weight: ["700"] })
 const notoSansJpText = Noto_Sans_Javanese({ subsets: ["latin"] })
 
-const bgColor = css`
-  background-color: #f9e5c0;
-`
+const bgColorStyle: React.CSSProperties = {
+  backgroundColor: "#f9e5c0",
+}
+const containerAreaStyle: React.CSSProperties = {
+  width: "100%",
+  aspectRatio: 91 / 55,
+}
+const profileAreaStyle: React.CSSProperties = {
+  marginRight: "1vw",
+}
+const headlineFontSizeStyle: React.CSSProperties = {
+  textAlign: "right",
+  fontSize: "6.25vw",
+  lineHeight: 1,
+}
+const subtitleFontSizeStyle: React.CSSProperties = {
+  textAlign: "right",
+  fontSize: "2.5vw",
+  lineHeight: "2rem",
+}
+
+const Logo = (): JSX.Element => (
+  <>
+    <Image
+      src="/logo.svg"
+      alt="takoba (signed by takoba)"
+      width={960}
+      height={360}
+      className="-rotate-3 p-4 absolute top-0 left-0"
+    />
+  </>
+)
 
 const Hero = (): JSX.Element => (
   <>
-    <div className="h-screen m-auto flex justify-end items-end" css={bgColor}>
-      <Image
-        src="/logo.svg"
-        alt="takoba (signed by takoba)"
-        width={960}
-        height={360}
-        className="-rotate-3 p-4 absolute top-0 left-0"
-      />
-      <div className="p-4 mr-16">
-        <h1 className={`${notoSansJpH1.className} text-6xl`}>オバラタクヤ</h1>
-        <p className={`${notoSansJpText.className}`}>
+    <div
+      className="m-auto flex justify-end items-end"
+      style={{ ...bgColorStyle, ...containerAreaStyle }}
+    >
+      <Logo />
+      <div className="p-4" style={{ ...profileAreaStyle }}>
+        <h1
+          className={`${notoSansJpH1.className}`}
+          style={{ ...headlineFontSizeStyle }}
+        >
+          オバラタクヤ
+        </h1>
+        <p
+          className={`${notoSansJpText.className} mr-2`}
+          style={{ ...subtitleFontSizeStyle }}
+        >
           Software Engineer OR UX Designer
         </p>
       </div>
