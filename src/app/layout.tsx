e39315279ead00eaf9fd25d5b7e6365e2480ac25
takoba <plane25@gmail.com>
2023-06-15 03:30:09 +0900

    :lipstick: Remove @emotion/react registry & global style settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import "./globals.css"
 import { Metadata } from "next"
-import EmotionRegistry from "@/lib/emotionRegistry"
-import GlobalStyles from "@/styles/globalStyles"
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <html lang="ja" className="min-w-min">
-      <GlobalStyles />
-      <body>
-        <EmotionRegistry>{children}</EmotionRegistry>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
