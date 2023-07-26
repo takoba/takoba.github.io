9dd694372e6d77ba43c6ef333fc8aacf63f8620e
takoba <plane25@gmail.com>
2023-07-27 04:48:15 +0900

    :heavy_plus_sign: Exec `npm install --save @kuma-ui/core @kuma-ui/next-plugin`
    - setup kuma-ui (arrange next.config.js)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const { withKumaUI } = require("@kuma-ui/next-plugin")
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withKumaUI({
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   output: process.env.GITHUB_ACTION !== undefined ? "export" : undefined,
-}
+})
