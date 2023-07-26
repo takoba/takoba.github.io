b7137a3eb020bf4a2c51c16eedf0eb7b621e2753
takoba <plane25@gmail.com>
2023-07-27 02:56:19 +0900

    :recycle: Replace to react-icons (from @mui/icons-material)

--- src/components/accountList.tsx
+++ src/components/accountList.tsx
@@ -1,9 +1,6 @@
-"use client"
-
 import React from "react"
 import NextLink from "next/link"
-import GitHubIcon from "@mui/icons-material/GitHub"
-import TwitterIcon from "@mui/icons-material/Twitter"
+import { FaGithub, FaTwitter } from "react-icons/fa"
 import ScrapboxIcon from "@/components/scrapboxIcon"
 
 const AccountList = (): JSX.Element => (
@@ -11,12 +8,12 @@ const AccountList = (): JSX.Element => (
     <ul className="flex justify-center align-middle">
       <li className="p-1">
         <NextLink href="https://github.com/takoba">
-          <GitHubIcon fontSize="large" />
+          <FaGithub size="34px" />
         </NextLink>
       </li>
       <li className="p-1">
         <NextLink href="https://twitter.com/takoba_">
-          <TwitterIcon fontSize="large" />
+          <FaTwitter size="34px" />
         </NextLink>
       </li>
       {/* アイコンが縦長なので、視覚調整としてpadding修正 */}
